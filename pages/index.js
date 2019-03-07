886f73dac8546dcdee214907993acf278216bd38
motohiro ishii <ishii@valus.co.jp>
2019-03-07 23:38:07 +0900

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about" style={{ fontSize: 20 }}>
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
